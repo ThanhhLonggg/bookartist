f9001702c5e8906ed89fa615b5f937d1b14e33f8
ThanhhLonggg <longntgch210780@fpt.edu.vn>
2023-08-19 17:02:52 +0700

    long2

--- app/Http/Controllers/ArtistController.php
+++ app/Http/Controllers/ArtistController.php
@@ -2,64 +2,80 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\Artist;
 use Illuminate\Http\Request;
 
 class ArtistController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $artists = Artist::all();
+        return view('home', ['artists' => $artists]);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function index2()
     {
-        //
+        $artists = Artist::all();
+        return view('adminArtist', ['artists' => $artists]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        $artist = Artist::find($id);
+        return view('artistDetail', ['artist' => $artist]);
+    }
+    public function index1()
+    {
+        $artists = Artist::all();
+        return view('artist', ['artists' => $artists]);
+    }
+    public function edit($id)
+    {
+        $artist = Artist::find($id);
+        return view('adminEdit', ['artist' => $artist]);
+    }
+    public function destroy($id)
+    {
+        $artist = Artist::find($id);
+        $artist->delete();
+        return redirect('/admin/artist')->with('success', 'artist deleted successfully.');
+    }
+    public function update(Request $request, $id)
+    {
+        $artist = Artist::find($id);
+        $artist->FirstName = $request->FirstName;
+        $artist->LastName = $request->LastName;
+        $artist->BirthDate = $request->BirthDate;
+        $artist->Img = $request->Img;
+        $artist->Price = $request->Price;
+        $artist->Description = $request->Description;
+        $artist->save();
+        return redirect('/admin/artist')->with('success', 'artist updated successfully.');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+   public function store(Request $request)
+    {       $request->validate([
+            'FirstName' => 'required',
+            'LastName' => 'required',
+            'BirthDate' => 'required',
+            'Img' => 'required',
+            'Price' => 'required',
+            'Description' => 'required',
+        ], [
+            'required' => 'Vui lòng điền đầy đủ thông tin.',
+        ]);
+       $artist = new Artist();
+        $artist->FirstName = $request->FirstName;
+        $artist->LastName = $request->LastName;
+        $artist->BirthDate = $request->BirthDate;
+        $artist->Img = $request->Img;
+        $artist->Price = $request->Price;
+        $artist->Description = $request->Description;
+        $artist->save();
+       return redirect('/admin/artist')->with('success', 'artist created successfully.');
+   }
+   public function create()
     {
-        //
+        return view('adminAdd');
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+ 
 }
