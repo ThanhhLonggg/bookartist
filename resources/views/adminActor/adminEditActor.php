327a1eb9484a6a6dfc2309809ba8d4650dd58aa8
QuyDoanVo <128034930+QuyDoanVo@users.noreply.github.com>
2023-08-26 09:30:09 +0700

    Add files via upload

--- resources/views/adminActor/adminEditActor.php
+++ resources/views/adminActor/adminEditActor.php
@@ -4,16 +4,24 @@
 <section class="content">
       <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
-            <form action="{{ route('actors.update', $actor->id) }}" method="POST">
+            <form action="{{ route('actors.update', $actor->id) }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('PUT')
             <div class="form-group">
-                <label for="FirstName">First Name</label>
-                <input type="text" class="form-control" id="FirstName" name="FirstName" value="{{ $actor->FirstName }}">
+                <label for="Name">Name</label>
+                <input type="text" class="form-control" id="Name" name="Name" value="{{ $actor->Name }}">
             </div>
             <div class="form-group">
-                <label for="LastName">Last Name</label>
-                <input type="text" class="form-control" id="LastName" name="LastName" value="{{ $actor->LastName}}">
+                <label for="Product">List of performances</label>
+                <input type="text" class="form-control" id="Product" name="Product" value="{{ $actor->Product}}">
+            </div>
+            <div class="form-group">
+                <label for="Sex">Sex</label>
+                <select class="form-control" id="Sex" name="Sex">
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                    <option value="Other">Other</option>
+                </select>
             </div>
             <div class="form-group">
                 <label for="BirthDate">Birth Date</label>
@@ -21,7 +29,8 @@
             </div>
             <div class="form-group">
                 <label for="Img">Image</label>
-                <input type="text" class="form-control" id="Img" name="Img" value="{{ $actor->Img}}">
+                <input type="file" class="form-control-file" id="Img" name="Img">
+                <img style="height: 100px;" src="{{ asset('images/' . $actor->Img) }}" alt="Current Image">
             </div>
             <div class="form-group">
                 <label for="Price">Price</label>
@@ -29,7 +38,7 @@
             </div>
             <div class="form-group">
                 <label for="Description">Description</label>
-                <textarea class="form-control" id="Description" name="Description" rows="3" value="">{{$actor->Description }}</textarea>
+                <textarea class="form-control" id="Description" name="Description" rows="3">{{ $actor->Description }}</textarea>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
             </form>
